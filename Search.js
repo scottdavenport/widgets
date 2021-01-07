5ed2842e6926384cc480cc62cf67236a6a8679ad
scottdavenport <scott@pinpoint.com>
2021-01-07 17:28:01 -0500

    Executing the Request from useEffect

--- Search.js
+++ Search.js
@@ -16,14 +16,17 @@ const Search = () => {
 				},
 			});
 		};
+
 		search();
 	}, [term]);
 
 	return (
-		<div className="ui form">
-			<div className="field">
-				<label>Enter Search Term</label>
-				<input className="input" value={term} onChange={(e) => setTerm(e.target.value)} />
+		<div>
+			<div className="ui form">
+				<div className="field">
+					<label>Enter Search Term</label>
+					<input value={term} onChange={(e) => setTerm(e.target.value)} className="input" />
+				</div>
 			</div>
 		</div>
 	);
