1b6f77ec74fdd4aeb4c1320900e3eebb679a5493
scottdavenport <scott@pinpoint.com>
2021-01-08 15:07:56 -0500

    Understanding Debounce

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,18 @@ import axios from 'axios';
 
 const Search = () => {
 	const [term, setTerm] = useState('programming');
+	const [debouncedTerm, setDebouncedTerm] = useState(term);
 	const [results, setResults] = useState([]);
+
+	useEffect(() => {
+		const timerId = setTimeout(() => {
+			setDebouncedTerm(term);
+		}, 1000);
+
+		return () => {
+			clearTimeout(timerId);
+		};
+	}, [term]);
 
 	useEffect(() => {
 		const search = async () => {
@@ -13,35 +24,14 @@ const Search = () => {
 					list: 'search',
 					origin: '*',
 					format: 'json',
-					srsearch: term,
+					srsearch: debouncedTerm,
 				},
 			});
 
 			setResults(data.query.search);
 		};
-
-		if (term && !results.length) {
-			search();
-		} else {
-			const timeoutId = setTimeout(() => {
-				if (term) {
-					search();
-				}
-			}, 1000);
-
-			console.log('Initial rendering or re-rendering');
-
-			// everything is run initially
-			// below is the clean-up function that is set on
-			// first render.  It will be run before any other code
-			// on the next rendering of component.
-			return () => {
-				clearTimeout(timeoutId);
-				console.log('CLEANUP');
-			};
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [term]);
+		search();
+	}, [debouncedTerm]);
 
 	const renderedResults = results.map((result) => {
 		return (
