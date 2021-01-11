818d1b4348525a1b20a920253f86067873457166
scottdavenport <scott@pinpoint.com>
2021-01-10 23:00:03 -0500

    Displaying Translated Text

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -2,21 +2,31 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Convert = ({ language, text }) => {
+	const [translated, setTranslated] = useState('');
+
 	useEffect(() => {
-		axios.post(
-			'https://translation.googleapis.com/language/translate/v2',
-			{},
-			{
-				params: {
-					q: text,
-					target: language.value,
-					key: 'AIzaSyAJ9TW84BIdqFnLM38jVfciaPRcgz5aJPI',
-				},
-			}
-		);
+		const doTranslation = async () => {
+			const { data } = await axios.post(
+				'https://translation.googleapis.com/language/translate/v2',
+				{},
+				{
+					params: {
+						q: text,
+						target: language.value,
+						key: 'AIzaSyAJ9TW84BIdqFnLM38jVfciaPRcgz5aJPI',
+					},
+				}
+			);
+			setTranslated(data.data.translations[0].translatedText);
+		};
+		doTranslation();
 	}, [language, text]);
 
-	return <div />;
+	return (
+		<div>
+			<h1 className="ui header">{translated}</h1>
+		</div>
+	);
 };
 
 export default Convert;
