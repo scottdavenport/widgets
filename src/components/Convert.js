f8682e826d6fba452216902c77cfe338d292ef09
scottdavenport <scott@pinpoint.com>
2021-01-10 22:49:44 -0500

    Using Google Translate API

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,8 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Convert = ({ language, text }) => {
 	useEffect(() => {
-		console.log('New language or text');
+		axios.post(
+			'https://translation.googleapis.com/language/translate/v2',
+			{},
+			{
+				params: {
+					q: text,
+					target: language.value,
+					key: 'AIzaSyAJ9TW84BIdqFnLM38jVfciaPRcgz5aJPI',
+				},
+			}
+		);
 	}, [language, text]);
 
 	return <div />;
